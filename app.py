b19b63e508b39c42a8903edd9d7017fccbcd148c
Linya Fernandez | ExpertCollege <lfernandez@expertcollege.com>
2024-04-22 16:15:57 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import altair as alt
+from dataProcessing import getData
+
+def main():
+
+    # I want to put a drop down where you can choose a view type
+    view = st.selectbox(
+        "Select a view:",
+        ("Project View", "Department View"),
+        index=0
+    )
+    st.divider()
+
+    if(view == "Project View"):
+        
+        st.title("Cost per Project for March")
+
+        costPerProject = getData.CostPerProject()
+
+        tableContainer = st.container(border=True)
+        tableContainer.subheader("Cost per Project")
+        tableContainer.write(alt.Chart(costPerProject.getDataframe()).mark_bar().encode(
+            x=alt.X(costPerProject.getX(), sort=None, title="Project"),
+            y=alt.Y(costPerProject.getY(), title="Cost"),
+        ).properties(
+            width=620,
+            height=500
+        ))
+        
+        tableContainer = st.container(border=True)
+        tableContainer.subheader("Project Table")
+        tableContainer.dataframe(costPerProject.getDataframe(), width=730, hide_index=True)
+
+    if(view == "Department View"):
+        st.title("Cost per Project per Team for March")
+
+        costPerProjectPerTeam = getData.CostPerProjectPerTeam()
+
+        team = st.selectbox(
+            "Select a team:",
+            costPerProjectPerTeam.getTeams(),
+            index=0
+        )
+
+        CostPerProjectPerTeamContainer = st.container(border=True)
+        CostPerProjectPerTeamContainer.subheader("Cost per Project for " + team)
+        CostPerProjectPerTeamContainer.write(alt.Chart(costPerProjectPerTeam.getDataframe(team)).mark_bar().encode(
+            x=alt.X(costPerProjectPerTeam.getX(), sort=None, title="Project"),
+            y=alt.Y(costPerProjectPerTeam.getY(), title="Cost"),
+        ).properties(
+            width=620,
+            height=500
+        ))
+
+        CostPerProjectPerTeamTableContainer = st.container(border=True)
+        CostPerProjectPerTeamTableContainer.subheader("Project Table")
+        CostPerProjectPerTeamTableContainer.dataframe(costPerProjectPerTeam.getDataframe(team), width=730, hide_index=True)
+
+
+
+
+
+
+        return None
+
+
+if __name__ == '__main__':
+    main()
---
+++ dataProcessing/getData.py
@@ -0,0 +1,82 @@
+import pandas as pd
+
+class CostPerProject():
+    x = 'Project'
+    y = 'Cost'
+    dataframe = ""
+    
+    def __init__(self):
+        self.dataframe = getOverallProjectCostsDataframe()
+
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def getDataframe(self):
+        return self.dataframe
+    
+
+class CostPerProjectPerTeam():
+    x = 'Project'
+    y = 'Cost'
+    dataframe = ""
+    teams = ""
+
+    def __init__(self):
+        self.dataframe, self.teams = getProjectCostPerTeamAndTeamNames()
+    
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+
+    def getTeams(self):
+        return self.teams
+    
+    def getDataframe(self, team):
+        dataframe = self.dataframe
+        return dataframe[dataframe['Space Name'] == team]
+
+
+def getProjectCostPerTeamAndTeamNames():
+    mergedProjectCostData = getMergedProjectCostDataframe()
+    projectCostPerDepartment = mergedProjectCostData.groupby(['Space Name', 'PROJECT CATEGORIES (FOR FINANCE)'])['Cost per Task'].sum().reset_index()
+    projectCostPerDepartment = projectCostPerDepartment.sort_values(by=['Space Name', 'Cost per Task'], ascending=[True, False])
+    projectCostPerDepartment.rename(columns={'PROJECT CATEGORIES (FOR FINANCE)': 'Project', 'Cost per Task': 'Cost'}, inplace=True)
+    teams = projectCostPerDepartment['Space Name'].unique()
+    return projectCostPerDepartment, teams
+
+def getOverallProjectCostsDataframe():
+    mergedProjectCostData = getMergedProjectCostDataframe()
+    projectCostsDataframe = mergedProjectCostData.groupby('PROJECT CATEGORIES (FOR FINANCE)')
+    projectCostsDataframe = projectCostsDataframe['Cost per Task'].sum()
+    projectCostsDataframe = projectCostsDataframe.reset_index()
+    projectCostsDataframe = projectCostsDataframe.sort_values(by='Cost per Task', ascending=False)
+    projectCostsDataframe.rename(columns={'PROJECT CATEGORIES (FOR FINANCE)': 'Project', 'Cost per Task': 'Cost'}, inplace=True)
+    return projectCostsDataframe
+
+def getMergedProjectCostDataframe():
+    hourRegistrationData = getHourRegistrationDataframe()
+    userData = getUserDataframe()
+    mergedProjectCostDataframe = pd.merge(hourRegistrationData, userData[['User ID', 'Hourly Cost']], on='User ID', how='inner')
+    mergedProjectCostDataframe['Cost per Task'] = mergedProjectCostDataframe['Hourly Cost'] * mergedProjectCostDataframe['Time Tracked Hours']
+    mergedProjectCostDataframe['Cost per Task'] = mergedProjectCostDataframe['Cost per Task'].round(2)
+    return mergedProjectCostDataframe
+
+def getHourRegistrationDataframe():
+    hourRegistrationData = pd.read_excel('./hour_registration_data.xlsx', sheet_name='data')
+    hourRegistrationData['Start'] = pd.to_datetime(hourRegistrationData['Start'])
+    hourRegistrationData['Stop'] = pd.to_datetime(hourRegistrationData['Stop'])
+    hourRegistrationData['Due Date'] = pd.to_datetime(hourRegistrationData['Due Date'])
+    hourRegistrationData['Start Date'] = pd.to_datetime(hourRegistrationData['Start Date'])
+    hourRegistrationData['Date Created'] = pd.to_datetime(hourRegistrationData['Date Created'])
+    hourRegistrationData['Time Tracked Hours'] = hourRegistrationData['Time Tracked'] / (1000 * 60 * 60)  # Converts from ms to hours
+    return hourRegistrationData
+
+def getUserDataframe():
+    userData = pd.read_excel('./users.xlsx', sheet_name='data')
+    userData['Hourly Cost'] = userData['Labor Cost per Month'] / (21*7.5) # Avg work days multiplied by 7.5 work hours -> Logic here can be improved, but leaving it here for testing
+    return userData
